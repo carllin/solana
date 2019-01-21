47ffb5fd9380a1d9f98c7f945e448c20c5fe47ce
Carl <carl@solana.com>
2019-01-21 01:01:40 -0800

    appease clippy

--- src/broadcast_service.rs
+++ src/broadcast_service.rs
@@ -192,6 +192,7 @@ pub struct BroadcastService {
 }
 
 impl BroadcastService {
+    #[allow(clippy::too_many_arguments)]
     fn run(
         bank: &Arc<Bank>,
         sock: &UdpSocket,
@@ -260,6 +261,7 @@ impl BroadcastService {
     /// WriteStage is the last stage in the pipeline), which will then close Broadcast service,
     /// which will then close FetchStage in the Tpu, and then the rest of the Tpu,
     /// completing the cycle.
+    #[allow(clippy::too_many_arguments)]
     pub fn new(
         bank: Arc<Bank>,
         sock: UdpSocket,
