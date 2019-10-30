566eba12d15a966dc21fab76d7b3a5956e190763
Carl <carl@solana.com>
2019-10-29 18:05:17 -0700

    Done

--- bench-tps/src/bench.rs
+++ bench-tps/src/bench.rs
@@ -634,22 +634,15 @@ pub fn airdrop_lamports<T: Client>(
         let (blockhash, _fee_calculator) = get_recent_blockhash(client);
         match request_airdrop_transaction(&drone_addr, &id.pubkey(), airdrop_amount, blockhash) {
             Ok(transaction) => {
-                let mut tries = 0;
-                loop {
-                    tries += 1;
-                    let signature = client.async_send_transaction(transaction.clone()).unwrap();
-                    let result = client.poll_for_signature_confirmation(&signature, 1);
-
-                    if result.is_ok() {
-                        break;
-                    }
-                    if tries >= 5 {
+                let signature = client.async_send_transaction(transaction).unwrap();
+                client
+                    .poll_for_signature_confirmation(&signature, 1)
+                    .unwrap_or_else(|_| {
                         panic!(
-                            "Error requesting airdrop: to addr: {:?} amount: {} {:?}",
-                            drone_addr, airdrop_amount, result
-                        )
-                    }
-                }
+                            "Error requesting airdrop: to addr: {:?} amount: {}",
+                            drone_addr, airdrop_amount
+                        );
+                    })
             }
             Err(err) => {
                 panic!(
