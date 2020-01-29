9a1c8d6860fbd08bd24545392f105ec721e20d8f
Carl <carl@solana.com>
2020-01-29 01:20:44 -0800

    Factor repair from gossip

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -12,6 +12,8 @@ use crate::{
     rpc_pubsub_service::PubSubService,
     rpc_service::JsonRpcService,
     rpc_subscriptions::RpcSubscriptions,
+    serve_repair::ServeRepair,
+    serve_repair_service::ServeRepairService,
     sigverify,
     storage_stage::StorageState,
     tpu::Tpu,
@@ -120,6 +122,7 @@ pub struct Validator {
     rpc_pubsub_service: Option<PubSubService>,
     transaction_status_service: Option<TransactionStatusService>,
     gossip_service: GossipService,
+    serve_repair_service: ServeRepairService,
     poh_recorder: Arc<Mutex<PohRecorder>>,
     poh_service: PohService,
     tpu: Tpu,
@@ -302,9 +305,16 @@ impl Validator {
 
         let gossip_service = GossipService::new(
             &cluster_info,
-            Some(blockstore.clone()),
             Some(bank_forks.clone()),
             node.sockets.gossip,
+            &exit,
+        );
+
+        let serve_repair = Arc::new(RwLock::new(ServeRepair::new(cluster_info.clone())));
+        let serve_repair_service = ServeRepairService::new(
+            &serve_repair,
+            Some(blockstore.clone()),
+            node.sockets.serve_repair,
             &exit,
         );
 
@@ -419,6 +429,7 @@ impl Validator {
         Self {
             id,
             gossip_service,
+            serve_repair_service,
             rpc_service,
             rpc_pubsub_service,
             transaction_status_service,
@@ -482,6 +493,7 @@ impl Validator {
         }
 
         self.gossip_service.join()?;
+        self.serve_repair_service.join()?;
         self.tpu.join()?;
         self.tvu.join()?;
         self.ip_echo_server.shutdown_now();
