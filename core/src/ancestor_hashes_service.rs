19df22268da1f7c6b1d7a5779027472742eb7748
Carl Lin <carl@solana.com>
2021-07-03 23:29:53 -0700

    Add updated duplicate broadcast test

--- core/src/ancestor_hashes_service.rs
+++ core/src/ancestor_hashes_service.rs
@@ -493,7 +493,7 @@ impl AncestorHashesService {
                     info!(
                         "{} Looking up status of dead slot {}, 
                         completed_nodes: {:?}
-                        stake: {}, 
+                        stake: {},
                         total_stake: {}, 
                         pct: {}",
                         id,
